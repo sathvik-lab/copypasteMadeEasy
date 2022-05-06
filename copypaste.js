9e625c37b7b479a0b4db256e0947ae033dedda5d
Sathvik <sathvikachanta003@gmail.com>
2022-05-06 22:16:01 +0530

    first commit

---
+++ copypaste.js
@@ -0,0 +1,7 @@
+(function() {
+    var allowPaste = function(e) {
+        e.stopImmediatePropagation();
+        return true;
+    };
+    document.addEventListener('paste', allowPaste, true);
+})();
